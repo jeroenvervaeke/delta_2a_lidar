711e80b239e74bdbb49e6e9647a3cfb17e56e8bc
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-08-02 21:47:45 +0200

    Improved packet_stream.rs documentation

--- src/packet_stream.rs
+++ src/packet_stream.rs
@@ -1,7 +1,13 @@
 use crate::packet::Packet;
 use async_trait::async_trait;
 
+/// Abstraction over a packet stream
+///
+/// Accept this abstraction instead of using `Lidar` directly.
+/// This way you can use both a real lidar and mocked lidar (or a measurements file)
 #[async_trait]
 pub trait PacketStream {
+    /// Reads the next lidar package.
+    /// Returns None if the stream has ended.
     async fn next(&mut self) -> Option<Packet>;
 }
