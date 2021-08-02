e98c607e0ef09795ce365a8bf58d1a5fd99c33e9
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-08-02 21:37:41 +0200

    Improved lib.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,49 @@
+//!  # Delta-2A lidar driver
+//! ## About
+//! This crate contains rust driver implementation for the [3irobotix delta-2A Lidar Sensor](https://www.banggood.com/custlink/KG3dehcdKd).
+//!
+//! ## Features
+//! - Read distance frames
+//! - Read lidar speed (WIP)
+//! - Read/write measurements to file + abstractions to mock sensor (behind '`file` feature)
+//!
+//! ## Dependencies
+//! This library uses the `serialport` crate which requires `libudev-dev` to be installed on your system.
+//! __On Ubuntu:__
+//! ```sh
+//! sudo apt-get update && sudo apt-get install -y libudev-dev
+//! ```
+//!
+//! ## Examples
+//! ### List al lidar sensors
+//! This simple example prints all found lidar sensors.
+//! ```no_run
+//! use delta_2a_lidar::Lidar;
+//!
+//! for sensor in Lidar::enumerate().unwrap() {
+//!     println!("Found lidar sensor: {:?}", sensor);
+//! }
+//! ```
+//!
+//! ### Read incoming packages
+//! This simple example prints all incoming packages from the first lidar sensor we find
+//! ```no_run
+//! use delta_2a_lidar::Lidar;
+//!
+//! // Get all lidars
+//! let mut lidar_names = Lidar::enumerate()?;
+//!
+//! // Take the first lidar
+//! let lidar_name = lidar_names.next().unwrap();
+//!
+//! // Open the lidar
+//! let mut lidar = Lidar::open(lidar_name).unwrap();
+//!
+//! // Read packages as long as the lidar produces packages
+//! while let Some(package) = lidar.next().await {
+//!    println!("Received package: {:?}", package);
+//!}
+//! ```
 pub mod crc;
 pub mod frame_parser;
 pub mod lidar;
