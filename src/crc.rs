7bd773bfa24099b0a4ac35828b6e19e11b492175
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-08-02 21:20:45 +0200

    Documented crc.rs

--- src/crc.rs
+++ src/crc.rs
@@ -1,25 +1,35 @@
+/// The CRC type is used to calculate the CRC of a lidar package.
+/// The underlying type for the CRC is an u16.
+///
+/// The CRC calculation is very straightforward. It's a sum of all bytes.
+/// When the underlying u16 overflows it is ignored (hence the use of overflowing_add)
 #[derive(Debug, PartialEq)]
 pub struct CRC(u16);
 
 impl CRC {
+    /// Create a new CRC from an u8
     pub fn new(value: u8) -> CRC {
         CRC(value as u16)
     }
 
+    /// Create a new CRC from an u16
     pub fn from_u16(value: u16) -> CRC {
         CRC(value)
     }
 
+    /// Calculate the CRC from an iterator of bytes
     pub fn from_bytes<'a, I: IntoIterator<Item = &'a u8>>(bytes: I) -> CRC {
         let iter = bytes.into_iter();
         iter.fold(CRC::new(0), |acc, current| acc.calculate_next(*current))
     }
 
+    /// Calculate the new CRC based on the current CRC and the next byte (u8)
     pub fn calculate_next(&self, value: u8) -> CRC {
         let next_value = self.0.overflowing_add(value as u16).0;
         CRC(next_value)
     }
 
+    /// Get the CRC value as an u16
     pub fn as_u16(&self) -> u16 {
         self.0
     }
