b4f9dde26090691c1ff7310b90361e2ad46f5d41
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-08-02 21:43:38 +0200

    Improved measurements_file.rs documentation

--- src/measurements_file.rs
+++ src/measurements_file.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, Lines};
 
+/// Open a file with measurements which were recorded using the `write` function
 pub async fn read(file_name: impl AsRef<Path>) -> Result<MeasurementReadFile> {
     let file = OpenOptions::new().read(true).open(file_name).await?;
     let buffered_reader = BufReader::new(file);
@@ -14,6 +15,7 @@ pub async fn read(file_name: impl AsRef<Path>) -> Result<MeasurementReadFile> {
     Ok(MeasurementReadFile::new(lines))
 }
 
+/// Open a measurements file for reading, the recorded packages can later be read using the `read` function
 pub async fn write(file_name: impl AsRef<Path>) -> Result<MeasurementWriteFile> {
     let file = File::create(file_name).await?;
     let buffered_writer = BufWriter::new(file);
@@ -21,6 +23,7 @@ pub async fn write(file_name: impl AsRef<Path>) -> Result<MeasurementWriteFile> 
     Ok(MeasurementWriteFile::new(buffered_writer))
 }
 
+/// File containing lidar measurements. This file implements `PacketStream` and can be used to mock a Lidar sensor
 pub struct MeasurementReadFile {
     lines: Lines<BufReader<File>>,
 }
@@ -40,6 +43,7 @@ impl PacketStream for MeasurementReadFile {
     }
 }
 
+/// A helper struct to write lidar measurements to a file
 pub struct MeasurementWriteFile {
     buffer: BufWriter<File>,
 }
@@ -49,6 +53,7 @@ impl MeasurementWriteFile {
         MeasurementWriteFile { buffer }
     }
 
+    /// Write a packet to the file
     pub async fn write(&mut self, packet: &Packet) -> Result<()> {
         let bytes = serde_json::to_string(packet)?;
 
